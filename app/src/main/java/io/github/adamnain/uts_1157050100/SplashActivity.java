b8caa1e2052a8f99b7811c878810715baaf36ec7
adamnain <adamdzulqarnain@gmail.com>
2018-11-03 11:03:53 +0700

    add splash screen

---
+++ app/src/main/java/io/github/adamnain/uts_1157050100/SplashActivity.java
@@ -0,0 +1,31 @@
+package io.github.adamnain.uts_1157050100;
+
+import android.content.Intent;
+import android.os.Handler;
+import android.support.v7.app.ActionBar;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SplashActivity extends AppCompatActivity {
+
+    public static int SPLASH_TIME_OUT = 1000;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+
+        ActionBar actionBar = getSupportActionBar();
+        actionBar.hide();
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent i = new Intent(SplashActivity.this, MainActivity.class);
+                startActivity(i);
+
+                finish();
+            }
+        }, SPLASH_TIME_OUT);
+    }
+}
